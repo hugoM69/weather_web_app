34cf077ee6349d43f399e42f7587619d515b3d49
hugoM69 <64667618+hugoM69@users.noreply.github.com>
2025-12-23 17:59:29 +0100

    Update locations.py

--- locations.py
+++ locations.py
@@ -1,10 +1,12 @@
 LOCATIONS = {
     "bilbao": {
         "lat": 43.2630,
-        "lon": -2.9350
+        "lon": -2.9350,
+        "aemet_id": "48020"
     },
     "pradoluengo": {
         "lat": 42.3245,
-        "lon": -3.2024
+        "lon": -3.2024,
+        "aemet_id": "09266"
     }
 }
